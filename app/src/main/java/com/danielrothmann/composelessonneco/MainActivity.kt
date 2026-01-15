f738868583d1fcdcb2abf2114e10095625eb19f2
Daniel Rotman <danilarothmann@gmail.com>
2026-01-15 19:56:07 +0700

    LazColumn - Text Expand

--- app/src/main/java/com/danielrothmann/composelessonneco/MainActivity.kt
+++ app/src/main/java/com/danielrothmann/composelessonneco/MainActivity.kt
@@ -1,89 +1,82 @@
 package com.danielrothmann.composelessonneco
 
+import ItemFood
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.core.view.WindowCompat
 import com.danielrothmann.composelessonneco.ui.theme.TsstColor1
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        // Для белого статус-бара - разрешаем отрисовку за системными барами
-        WindowCompat.setDecorFitsSystemWindows(window, false)
-
         enableEdgeToEdge()
         setContent {
-            MaterialTheme {
-                showMenu()
-            }
+            showMenu()
         }
     }
 
+    @Preview
     @Composable
     private fun showMenu() {
         val menu = createMenu()
 
-        Box(
+        LazyColumn(
             modifier = Modifier
-                .fillMaxWidth()
-                .height(140.dp)  // Фиксированная высота меню
-                .background(Color.White)  // Белый фон для Box (будет виден в статус-баре)
+                .fillMaxSize()
+                .statusBarsPadding()
+                .background(TsstColor1)  // Тестовый фон для LazyRow
+                .padding(horizontal = 8.dp, vertical = 8.dp),  // Отступы по бокам и сверху/снизу
+            verticalArrangement = Arrangement.spacedBy(6.dp), //  Отступы между элементами
         ) {
-            // тестовый фон только для LazyRow
-            LazyRow(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(120.dp)  // Высота LazyRow меньше чем у Box
-                    .align(Alignment.BottomCenter)  // Размещаем внизу Box
-                    .background(TsstColor1)  // Тестовый фон для LazyRow
-                    .padding(horizontal = 8.dp, vertical = 16.dp)  // Отступы по бокам и сверху/снизу
-
-            ) {
-                itemsIndexed(menu) { index, itemShow ->
-                    // Добавляем отступы между элементами
-                    ItemFoodRow(
-                        itemListFood = itemShow,
-                        modifier = Modifier.padding(
-                            start = if (index == 0) 0.dp else 8.dp,  // Первому элементу отступ слева не нужен
-                            end = if (index == menu.lastIndex) 0.dp else 8.dp  // Последнему справа не нужен
-                        )
-                    )
-                }
+            itemsIndexed(menu) { index, itemShow ->
+                ItemFood(
+                    itemListFood = itemShow
+                )
             }
         }
     }
-
-
-    // Функция возвращает список, а не один элемент
-    private fun createMenu(): List<ModelFood> {
-        return listOf(
-            ModelFood(R.drawable.im_combo, "Combo"),
-            ModelFood(R.drawable.im_desert, "Desert"),
-            ModelFood(R.drawable.im_french_fries, "French Fries"),
-            ModelFood(R.drawable.im_pasta, "Pasta"),
-            ModelFood(R.drawable.im_pizza, "Pizza"),
-            ModelFood(R.drawable.im_salad, "Salat"),
-            ModelFood(R.drawable.im_sushi, "Sushi")
-        )
-    }
 }
+
+
+// Функция возвращает список, а не один элемент
+private fun createMenu(): List<ModelFood> {
+    return listOf(
+        ModelFood(R.drawable.im_combo, "Combo", "SomeText"),
+        ModelFood(R.drawable.im_desert, "Desert", "SomeTextSomeTextSomeTextSomeText" +
+                "SomeTextSomeTextSomeTextSomeTextSomeTextSomeTextSomeTextSomeTextSomeText" +
+                "SomeTextSomeTextSomeTextSome" +
+                "TextSomeTextSomeTextSomeTextSomeTextSomeTextSomeTextSomeT" +
+                "extSomeTextSomeTextSomeTextSomeTextSomeTextSomeTextSomeTextSome" +
+                "TextSomeTextSomeTextSomeTextSomeTextSomeTextSomeTextSomeTextSomeTex" +
+                "tSomeTextSomeTextSomeTextSomeTextSomeTextSomeTextSomeTextSomeTextSomeTextSomeTextSo" +
+                "meTextSomeTextSomeTextSomeTextSomeTextSomeTextSomeTextSomeTextSomeTextSomeTextSomeTex" +
+                "tSomeTextSomeTextSomeTextSomeTextSomeTextSomeTextSomeTextSomeTextSom" +
+                "eTextSomeTextSomeTextSomeTextSomeTextSomeTextSomeTextSomeTextSom" +
+                "eTextSomeTextSomeTextSomeTextSomeTextSomeTextSomeTextSomeTextSo" +
+                "meTextSomeTextSomeTextSomeTextSomeTextSomeTextSomeTextSomeTextSomeT" +
+                "extSomeTextSomeTextSomeTextSomeTextSomeTextSomeTextSomeTextSomeText"),
+        ModelFood(R.drawable.im_french_fries, "French Fries", "SomeText"),
+        ModelFood(R.drawable.im_pasta, "Pasta", "SomeText"),
+        ModelFood(R.drawable.im_pizza, "Pizza", "SomeText"),
+        ModelFood(R.drawable.im_salad, "Salat", "SomeText"),
+        ModelFood(R.drawable.im_sushi, "Sushi", "SomeText")
+    )
+}
+
 
 
 
--- app/src/main/java/com/danielrothmann/composelessonneco/ModelFood.kt
+++ app/src/main/java/com/danielrothmann/composelessonneco/ModelFood.kt
@@ -3,4 +3,5 @@ package com.danielrothmann.composelessonneco
 data class ModelFood(
     val imageId: Int,
     val title: String,
+    val content: String
 )
